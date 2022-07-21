ba8f0369d64b9c76214e6515ce6f6b5952585384
Shayne M <nshayne.mcpherson@gmail.com>
2022-07-21 13:59:37 -0500

    list of tags example

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -3,15 +3,26 @@ import React, {Component} from "react";
 export default class Counter extends Component {
     state = {
         count: 0,
+        tags: ['tag 1', 'tag2', 'tag3']
     };
 
     render() {
         return (
             <div>
-                <span className="badge-primary m-2"> {this.formatCount()} </span>
-                <button className="btn btn-secondary btn-sm"> Increment</button>
+             {this.renderTags()}
             </div>
         );
+    }
+
+    renderTags() {
+        if (this.state.tags.length === 0) return <p> There are no tags!</p>;
+        return    <ul>{this.state.tags.map(tag => <li key = {tag}>{tag}</li>)}</ul>
+    }
+
+    getBadgeClasses() {
+        let classes = "bg m-2 bg-";
+        classes += (this.state.count === 0) ? "warning" : "primary";
+        return classes;
     }
 
     formatCount() {
