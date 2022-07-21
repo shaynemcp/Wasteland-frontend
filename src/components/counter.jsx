f05b58fbf707eeb18ff46c966145ca41a25ba3ce
Shayne M <nshayne.mcpherson@gmail.com>
2022-07-21 13:05:20 -0500

    counter component added

---
+++ src/components/counter.jsx
@@ -0,0 +1,22 @@
+import React, {Component} from "react";
+
+export default class Counter extends Component {
+    state = {
+        count: 0,
+    };
+
+    render() {
+        return (
+            <div>
+                <span className="badge-primary m-2"> {this.formatCount()} </span>
+                <button className="btn btn-secondary btn-sm"> Increment</button>
+            </div>
+        );
+    }
+
+    formatCount() {
+        const {count} = this.state;
+        return count === 0 ? 'zero' : count;
+    }
+}
+
