6bcc5da3f8250aa3b4fe346988595840dc4f01cb
Shayne McPherson <shayne@Shaynes-MBP.lan>
2022-07-25 00:11:19 -0500

    header/intro component added

--- src/index.js
+++ src/index.js
@@ -9,12 +9,14 @@ import BadLuck from './components/badLuck';
 import RollingStone from './components/rollingStone';
 import Addictions from './components/addictions';
 import JackieBrown from './components/jackieBrown';
+import Intro from './components/intro';
 // import Song from './components/song';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     {/* <Counter /> */}
+    <Intro />
     <Addictions />
     <Angel />   
     <AllMine />
