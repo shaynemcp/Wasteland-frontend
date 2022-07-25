57f3424c4e1d6dae800d6170a2383c4f6cc21c49
Shayne McPherson <shayne@Shaynes-MacBook-Pro.local>
2022-07-25 14:14:51 -0500

    change Like & Dislike a-tags to buttons as per error msg on npm run build

--- src/components/intro.jsx
+++ src/components/intro.jsx
@@ -15,13 +15,13 @@ render() {
         
         <div>
             {/* Intro to webpage  */}
-            <div class="row py-lg-5">
+                <div class="row py-lg-5">
                     <div class="col-lg-6 col-md-8 mx-auto">
                     <h1 class="fw-light">Wasteland! the album by Brent Faiyaz</h1>
                     <p class="lead text-muted">Welcome to Wasteland! the album. I've been anticipating this project for two years now and would like to showcase my favorites. On this page, you can vote for yours and we will be able to see which songs are the best!</p>
                     <p>
-                        <a href="#" class="btn btn-primary my-2">Like</a>
-                        <a href="#" class="btn btn-secondary my-2">Dislike</a>
+                        <button class="btn btn-primary my-2">Like</button>
+                        <button class="btn btn-secondary my-2">Dislike</button>
                     </p>
                     </div>
                 </div>
